04b0ba1873f5119695660c64c22c73b40fc5518a
bebo <bobosh.awad@gmail.com>
2021-12-05 13:24:37 +0200

    Initial commit

---
+++ app/src/main/java/com/plan/kemet2/io/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.plan.kemet2.io;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.plan.kemet2.MainActivity;
+import com.plan.kemet2.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+        {
+        }
+
+
+    }
+}
